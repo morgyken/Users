1c2c331984efeab9945e5f9ed383858f5dad452e
Samuel Odhiambo <sodhiambo@collabmed.com>
2016-10-07 11:42:50 +0300

    Add role fix

--- Resources/views/roles/create.blade.php
+++ Resources/views/roles/create.blade.php
@@ -1,22 +1,16 @@
-@extends('layouts.master')
+@extends('layouts.app')
 
-@section('content-header')
-    <h1>New Role</h1>
-    <ol class="breadcrumb">
-        <li><a href="{{ URL::route('dashboard.index') }}"><i class="fa fa-dashboard"></i> {{ trans('core::core.breadcrumb.home') }}</a></li>
-        <li class=""><a href="{{ URL::route('admin.user.role.index') }}">{{ trans('user::roles.breadcrumb.roles') }}</a></li>
-        <li class="active">{{ trans('user::roles.breadcrumb.new') }}</li>
-    </ol>
-@stop
+@section('content_title','New Role')
+@section('content_description','Create a new user group')
 
 @section('content')
-{!! Form::open(['route' => 'admin.user.role.store', 'method' => 'post']) !!}
+{!! Form::open(['route' => 'users.role.store', 'method' => 'post']) !!}
 <div class="row">
     <div class="col-md-12">
         <div class="nav-tabs-custom">
             <ul class="nav nav-tabs">
-                <li class="active"><a href="#tab_1-1" data-toggle="tab">{{ trans('user::roles.tabs.data') }}</a></li>
-                <li class=""><a href="#tab_2-2" data-toggle="tab">{{ trans('user::roles.tabs.permissions') }}</a></li>
+                <li class="active"><a href="#tab_1-1" data-toggle="tab">Role Info</a></li>
+                <li class=""><a href="#tab_2-2" data-toggle="tab">Permissions</a></li>
             </ul>
             <div class="tab-content">
                 <div class="tab-pane active" id="tab_1-1">
@@ -24,15 +18,15 @@
                         <div class="row">
                             <div class="col-sm-6">
                                 <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                                    {!! Form::label('name', trans('user::roles.form.name')) !!}
-                                    {!! Form::text('name', Input::old('name'), ['class' => 'form-control', 'data-slug' => 'source', 'placeholder' => trans('user::roles.form.name')]) !!}
+                                    {!! Form::label('name', 'Role name') !!}
+                                    {!! Form::text('name', old('name'), ['class' => 'form-control', 'data-slug' => 'source', 'placeholder' => 'Role name']) !!}
                                     {!! $errors->first('name', '<span class="help-block">:message</span>') !!}
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="form-group{{ $errors->has('slug') ? ' has-error' : '' }}">
-                                    {!! Form::label('slug', trans('user::roles.form.slug')) !!}
-                                    {!! Form::text('slug', Input::old('slug'), ['class' => 'form-control slug', 'data-slug' => 'target', 'placeholder' => trans('user::roles.form.slug')]) !!}
+                                    {!! Form::label('slug', 'Slug') !!}
+                                    {!! Form::text('slug', old('slug'), ['class' => 'form-control slug', 'data-slug' => 'target', 'placeholder' => 'Slug']) !!}
                                     {!! $errors->first('slug', '<span class="help-block">:message</span>') !!}
                                 </div>
                             </div>
@@ -40,12 +34,12 @@
                     </div>
                 </div>
                 <div class="tab-pane" id="tab_2-2">
-                    @include('user::admin.partials.permissions-create')
+                    @include('users::partials.permissions-create')
                 </div>
                 <div class="box-footer">
-                    <button type="submit" class="btn btn-primary btn-flat">{{ trans('user::button.create') }}</button>
-                    <button class="btn btn-default btn-flat" name="button" type="reset">{{ trans('core::core.button.reset') }}</button>
-                    <a class="btn btn-danger pull-right btn-flat" href="{{ URL::route('admin.user.role.index')}}"><i class="fa fa-times"></i> {{ trans('user::button.cancel') }}</a>
+                    <button type="submit" class="btn btn-primary btn-flat">Create</button>
+                    <button class="btn btn-default btn-flat" name="button" type="reset">Clear</button>
+                    <a class="btn btn-danger pull-right btn-flat" href="{{ route('users.role.index')}}"><i class="fa fa-times"></i> Cancel</a>
                 </div>
             </div>
         </div>
@@ -54,26 +48,26 @@
 {!! Form::close() !!}
 @stop
 @section('footer')
-    <a data-toggle="modal" data-target="#keyboardShortcutsModal"><i class="fa fa-keyboard-o"></i></a> &nbsp;
+<a data-toggle="modal" data-target="#keyboardShortcutsModal"><i class="fa fa-keyboard-o"></i></a> &nbsp;
 @stop
 @section('shortcuts')
-    <dl class="dl-horizontal">
-        <dt><code>b</code></dt>
-        <dd>{{ trans('user::roles.navigation.back to index') }}</dd>
-    </dl>
+<dl class="dl-horizontal">
+    <dt><code>b</code></dt>
+    <dd> Back to index</dd>
+</dl>
 @stop
 @section('scripts')
 <script>
-$( document ).ready(function() {
-    $('input[type="checkbox"].flat-blue, input[type="radio"].flat-blue').iCheck({
-        checkboxClass: 'icheckbox_flat-blue',
-        radioClass: 'iradio_flat-blue'
+    $(document).ready(function () {
+        $('input[type="checkbox"].flat-blue, input[type="radio"].flat-blue').iCheck({
+            checkboxClass: 'icheckbox_flat-blue',
+            radioClass: 'iradio_flat-blue'
+        });
+        $(document).keypressAction({
+            actions: [
+                {key: 'b', route: "<?= route('users.role.index') ?>"}
+            ]
+        });
     });
-    $(document).keypressAction({
-        actions: [
-            { key: 'b', route: "<?= route('admin.user.role.index') ?>" }
-        ]
-    });
-});
 </script>
 @stop
